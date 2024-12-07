23524a6fb4af27f275e2ac6dfcbc8cea0e5e7ced
darif-aya <80434528+darif-aya@users.noreply.github.com>
2024-12-07 09:01:13 +0100

    Update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const SentimentAnalyzerModule = require('./sentimentAnalyzer');
 
 const sentimentAnalyzer = new SentimentAnalyzerModule();
 
-const result1 = sentimentAnalyzer.analyzeSentiment("bad product");
+const result1 = sentimentAnalyzer.analyzeSentiment("the perfect product");
 const result2 = sentimentAnalyzer.analyzeSentiment("the worst service.");
 const result3 = sentimentAnalyzer.analyzeSentiment("I'm really disappointed with this product.");
 
